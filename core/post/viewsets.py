fee11ef0d0bfeb6a1f3f3de8d66a518a5647efa0
matti909 <matias.saantiago@gmail.com>
2024-05-11 16:30:11 -0300

    likes actions added

--- core/post/viewsets.py
+++ core/post/viewsets.py
@@ -1,15 +1,17 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from core.abstract.viewsets import AbstractViewSet
+from core.auth.permissions import UserPermission
 from core.post.models import Post
 from core.post.serializers import PostSerializer
 
 
 class PostViewSet(AbstractViewSet):
     http_method_names = ["post", "get", "put", "delete"]
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (UserPermission,)
     serializer_class = PostSerializer
 
     def get_queryset(self):
@@ -27,3 +29,25 @@ class PostViewSet(AbstractViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @action(methods=["post"], detail=True)
+    def like(self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user
+
+        user.like(post)  # type: ignore
+
+        serializer = self.serializer_class(post)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(methods=["post"], detail=True)
+    def remove_like(self, request, *args, **kwargs):
+        post = self.get_object()
+        user = self.request.user
+
+        user.remove_like(post)  # type: ignore
+
+        serializer = self.serializer_class(post)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- core/user/models.py
+++ core/user/models.py
@@ -61,6 +61,8 @@ class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     created = models.DateTimeField(auto_now=True)
     updated = models.DateTimeField(auto_now_add=True)
 
+    posts_liked = models.ManyToManyField("core_post.Post", related_name="liked_by")
+
     is_staff = models.BooleanField(default=False)
 
     USERNAME_FIELD = "email"
@@ -74,3 +76,15 @@ class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     @property
     def name(self):
         return f"{self.first_name} {self.last_name}"
+
+    def like(self, post):
+        """Like `post` if it hasn't been done yet"""
+        return self.posts_liked.add(post)
+
+    def remove_like(self, post):
+        """Remove a like from a `post`"""
+        return self.posts_liked.remove(post)
+
+    def has_liked(self, post):
+        """Return True if the user has liked a `post`; else False"""
+        return self.posts_liked.filter(pk=post.pk).exists()
